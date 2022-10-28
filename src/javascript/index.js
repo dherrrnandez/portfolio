2468c520a6ec878f575b98ca0c80f02a5cc97665
Daniel Hernandez <daniherrrnandez@gmail.com>
2022-10-28 13:21:04 -0300

    code splitting

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,27 +1,15 @@
+import arTime from "./arTime.js"
+const menu = document.getElementById("menu")
+const hamMenu = document.getElementById("hamIcon")
+const menuItem = document.querySelectorAll(".menu_item")
+const workImg = document.querySelectorAll(".work_img")
+const workAnchor = document.querySelectorAll(".work_a")
+
 window.scrollTo(0, 0)
-
-const clock = document.getElementById("clock")
-
-const arTime = () => {
-    const date = new Date()
-
-    let time = date.toLocaleString("es-AR", {
-        timeZone: "America/Argentina/Buenos_Aires",
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true
-    }) //time === "00:00 x. m."
-
-    time = time.slice(0, 7) + time.slice(9, 10) // from "p. m." to "pm"
-    time = time.toUpperCase()
-    clock.innerText = `ARGENTINA ${time}`
-}
 
 arTime()
 setInterval(arTime, 30000)
 
-const hamMenu = document.getElementById("hamIcon")
-const menu = document.getElementById("menu")
 
 let menuIsActive = false
 
@@ -37,17 +25,12 @@ hamMenu.addEventListener("click", () => {
     }
 })
 
-const menuItem = document.querySelectorAll(".menu_item")
-
 menuItem.forEach(item => {
     item.addEventListener("click", () => {
         menuIsActive = false
         menu.classList.remove("active")
     })
 })
-
-const workImg = document.querySelectorAll(".work_img")
-const workAnchor = document.querySelectorAll(".work_a")
 
 workAnchor.forEach(item => {
     item.addEventListener("click", () => {
